944bf2fa9c5db14a929252de7805448ad5696621
Bradley Agyei <Bradley-Agyei@users.noreply.github.com>
2022-01-29 16:24:32 -0500

    rendered templates to login nad signup pages.

--- website/auth.py
+++ website/auth.py
@@ -1,20 +1,20 @@
-from flask import Blueprint #Blueprint is used to organize the different views. Views can be defined in multiple files.
+from flask import Blueprint, render_template #Blueprint is used to organize the different views. Views can be defined in multiple files.
 
 auth = Blueprint('auth', __name__)
 
 #Route users to login page
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 
 #Route users to logout page
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("home.html")
 
 
 #Route users to signup page
-@auth.route('/signup')
+@auth.route('/sign_up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
